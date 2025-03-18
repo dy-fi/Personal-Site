f6436ca8683b64d077d5c569d337dd6ea3ebac08
Dylan Finn <dylan@Dylans-MacBook-Pro.local>
2025-03-18 14:43:15 -0700

    remove nodemailer infra

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,5 @@
 const fs = require('fs')
 const path = require('path')
-// const nodemailer = require('nodemailer')
-
-// var transporter = nodemailer.createTransport({
-//     service: '',
-//     auth: {
-//         xoauth2: generator,
-//     }
-// })
 
 module.exports = (app) => {
     // index
@@ -22,28 +14,5 @@ module.exports = (app) => {
 
         res.render('portfolio', {projects})
     })
-
-    // app.post('/email', (req, res) => {
-    //     var email = req.body.email
-    //     var subject = req.body.subject
-    //     var content = req.body.content
-
-    //     var mailOptions = {
-    //         from: email,
-    //         to: process.env.ADMIN_EMAIL,
-    //         subject: subject,
-    //         text: content,
-    //     }
-
-    //     transporter.sendMail(mailOptions, function(err, info) {
-    //         if (err) {
-    //             console.log(err);
-    //         } else {
-    //             res.redirect('/', 200);
-    //         }
-
-
-    //     })
-    // });
 }
 
